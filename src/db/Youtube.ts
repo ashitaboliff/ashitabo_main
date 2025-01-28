17d3b636d7bf53d7269e8589ea858193f603b716
watabegg <roy_wtnb@outlook.jp>
2025-01-28 15:00:30 +0900

    ん～これで

--- src/db/Youtube.ts
+++ src/db/Youtube.ts
@@ -62,45 +62,54 @@ export const upsertAccessToken = async ({
 }
 
 export const createPlaylistBatch = async (playlists: Playlist[]) => {
-	await Promise.all(
-		playlists.map(async (playlist) => {
-			await prisma.playlist.upsert({
-				where: { playlistId: playlist.playlistId },
-				update: {
-					title: playlist.title,
-					link: playlist.link,
-					liveDate: playlist.liveDate,
-				},
-				create: {
-					playlistId: playlist.playlistId,
-					title: playlist.title,
-					link: playlist.link,
-					liveDate: playlist.liveDate,
-				},
-			})
-
-			await Promise.all(
-				playlist.videos.map(async (video) => {
-					await prisma.video.upsert({
-						where: { videoId: video.videoId },
+	const createPlaylistBatch = async (
+		playlists: Playlist[],
+		batchSize: number,
+	) => {
+		for (let i = 0; i < playlists.length; i += batchSize) {
+			const batch = playlists.slice(i, i + batchSize)
+			await prisma.$transaction(async (prisma) => {
+				for (const playlist of batch) {
+					await prisma.playlist.upsert({
+						where: { playlistId: playlist.playlistId },
 						update: {
-							title: video.title,
-							link: video.link,
-							playlistId: playlist.playlistId,
-							liveDate: video.liveDate,
+							title: playlist.title,
+							link: playlist.link,
+							liveDate: playlist.liveDate,
 						},
 						create: {
-							videoId: video.videoId,
-							title: video.title,
-							link: video.link,
-							liveDate: video.liveDate,
 							playlistId: playlist.playlistId,
+							title: playlist.title,
+							link: playlist.link,
+							liveDate: playlist.liveDate,
 						},
 					})
-				}),
-			)
-		}),
-	)
+
+					for (const video of playlist.videos) {
+						await prisma.video.upsert({
+							where: { videoId: video.videoId },
+							update: {
+								title: video.title,
+								link: video.link,
+								playlistId: playlist.playlistId,
+								liveDate: video.liveDate,
+							},
+							create: {
+								videoId: video.videoId,
+								title: video.title,
+								link: video.link,
+								liveDate: video.liveDate,
+								playlistId: playlist.playlistId,
+							},
+						})
+					}
+				}
+			})
+		}
+	}
+
+	const batchSize = 100
+	await createPlaylistBatch(playlists, batchSize)
 }
 
 export const getPlaylistById = async (id: string) => {
