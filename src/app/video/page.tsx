b5be27d80732973a4c643d5b515f188a4a403bd2
watabegg <roy_wtnb@outlook.jp>
2025-01-28 13:17:15 +0900

    はい

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -2,9 +2,14 @@
 
 import { notFound } from 'next/navigation'
 import YoutubeMainPage from '@/components/video/YoutubeMainPage'
+import { Suspense } from 'react'
 
 const Page = async () => {
-	return <YoutubeMainPage />
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<YoutubeMainPage />
+		</Suspense>
+	)
 }
 
 export default Page
